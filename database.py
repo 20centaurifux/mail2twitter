b79490274c3547e1d88e7efe3e355e7d1357f1f2
lordkefir <lord-kefir@arcor.de>
2012-09-11 08:03:26 +0200

    queue database function implementation

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 import sqlite3, os
 
+PUBLISH_TWEET = 0
+FOLLOW_USER   = 1
+UNFOLLOW_USER = 2
+
 class Database:
 	def __init__(self, directory):
 		filename = os.path.join(directory, 'mail2twitter.db')
@@ -41,6 +45,29 @@ class Database:
 			users.append(row)
 
 		return users
+
+	def mapUser(self, username):
+		self.__cursor.execute('SELECT ID, Firstname, Lastname, Email, Blocked FROM User WHERE Username=?', (username, ))
+
+		result = self.__cursor.fetchone()
+
+		if not result is None:
+			return int(result[0])
+
+		return None
+
+	def appendToQueue(self, userId, typeId, text, ctime):
+		self.__cursor.execute('INSERT INTO Queue (UserId, TypeId, Text, CTime) VALUES (?, ?, ?, ?)', (userId, typeId, text, ctime))
+
+	def getQueue(self):
+		items = []
+
+		self.__cursor.execute('SELECT Queue.ID, username, email, TypeID, Text, CTime FROM Queue INNER JOIN User ON UserID=User.ID ORDER BY CTime')
+
+		for row in self.__cursor.fetchall():
+			items.append(row)
+
+		return items
 
 	def __createTables__(self):
 		self.__cursor.execute('CREATE TABLE IF NOT EXISTS User (ID INT PRIMARY KEY, Username VARCHAR(64) UNIQUE, Firstname VARCHAR(64) NOT NULL, ' \
