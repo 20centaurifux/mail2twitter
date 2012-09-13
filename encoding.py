4f3f57f73419aa7a4d2047646b7461cc347d3320
lordkefir <lord-kefir@arcor.de>
2012-09-13 17:30:59 +0200

    implemented function to convert strings to different character encodings

---
+++ encoding.py
@@ -0,0 +1,40 @@
+"""
+	project............: mail2twitter
+	version............: 0.1
+	date...............: 09/12
+	copyright..........: Sebastian Fedrau
+	email..............: lord-kefir@arcor.de
+
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+
+	This synchronziation procedure works only file-based. It will not upload
+	empty folders or remove empty folders on the remote site.
+"""
+
+# -*- coding: utf-8 -*-
+import icu
+
+def encode(text, new_coding='UTF-8'):
+	coding = icu.CharsetDetector(text).detect().getName()
+  
+	if new_coding.upper() != coding.upper():
+		text = unicode(text, coding).encode(new_coding)
+
+	return text
